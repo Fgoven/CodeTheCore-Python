b870a434ce92bce355635e5624106b2208b284f0
Fgoven <fatmagoven97@icloud.com>
2025-10-13 13:49:08 +0300

    comment, data

---
+++ _python_pure_13_10_2025/_01_print_comment_type.py
@@ -0,0 +1,19 @@
+# Single Comment
+
+"""
+Multiple Comment
+"""
+##########################################################
+############print#########################################
+
+# string
+print("Python Öğreniyorum")
+
+# Virgüllü sayı
+print(14.34)
+
+# Tam sayı
+print(14563)
+
+# string'den birden fazla değerse
+print("Python","Java","JS", "Nodejs")
