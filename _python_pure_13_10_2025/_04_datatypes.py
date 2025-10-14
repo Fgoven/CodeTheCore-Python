c9932dd7ca0b79173d8ec67e2f17c142cf6769ed
Fgoven <fatmagoven97@icloud.com>
2025-10-14 10:31:46 +0300

    datatypes hint types

--- _python_pure_13_10_2025/_04_datatypes.py
+++ _python_pure_13_10_2025/_04_datatypes.py
@@ -12,13 +12,38 @@ print(f"number1: {number1} number2:{number2} PI:{PI}")
 ## types ##
 
 print(f"number1: {number1} ==> Türü: {type(number1)}")
-
+print(f"PI: {PI} ==> Türü: {type(PI)}")
 ## string ##
 name = "Fatma"
 surname = "Göven"
 
-print(f"Adım: {name} Soyadım: {surname}")
+print(f"Adım: {name}, Soyadım: {surname}")
 
 ## boolean True/False ##
 is_login = True
 print(f"Login mi? {is_login} ==> Türü: {type(is_login)}")
+
+## List ##
+
+my_list = [1, 2, 3, 4, 5, 6, True, "Malatya", 51.56]
+print(f"Liste: {my_list}, ==> Türü: {type(my_list)}")
+
+## Tuple ##
+# Tuple'daki değerler değiştirilemez, immutable
+my_tuple = (1, 2, 3, 4, 5, 6, True,"Malatya", 51.56)
+print(f"Tuple: {my_tuple}, ==> Türü: {type(my_tuple)}")
+
+## Set ##
+# unique yapı her eleman benzersizdir
+
+my_set = { 2, 3, 4, 5, 6,"Malatya", "Malatya", 51.56, 2, 6, 5, 4, 2, 3, True, True, "Malatya", False}
+print(f"Set: {my_set}, ==> Türü: {type(my_set)}")
+
+## Dictionary
+
+my_dict = {
+    "name": "Fatma",
+    "surname": "Göven",
+    "is_login":True
+}
+print(f"Dict: {my_dict}, ==> Türü: {type(my_dict)}")
