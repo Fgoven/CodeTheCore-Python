4b8f0e7b9c9ba40f3c67a7186b27e35b187117e5
Fgoven <fatmagoven97@icloud.com>
2025-10-14 12:20:25 +0300

    typecast,input ok math created

---
+++ _python_pure_13_10_2025/_07_datateypecast.py
@@ -0,0 +1,19 @@
+
+# Type Casting veri türlerinin birbirine dönüştürülmesi
+
+# int()
+# str()
+# float()
+# bool()
+
+number1 ="44"
+print(f"Sayı {number1} ==> type{type(number1)}")
+number1 = int(number1)
+print(f"Sayı {number1} ==> type{type(number1)}")
+number1 = float(number1)
+print(f"Sayı {number1} ==> type{type(number1)}")
+x = "True"
+number1 = bool(x)
+print(f"Sayı {number1} ==> type{type(number1)}")
+
+
---
+++ _python_pure_13_10_2025/_08_input.py
@@ -0,0 +1,4 @@
+number1 = input("Lütfen 1.sayıyı girin: ")
+number2 = input("Lütfen 2. sayıyı girin: ")
+number3 = int(number1) + int(number2)
+print(f"1.sayı: {number1} type {type(number1)} 2.sayı: {number2} type {type(number2)} Toplam = {number3} type {type(number3)} ")
